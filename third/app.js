0f5e767c20f413fa5cf5b5efc65afe9b48c10d67
Adam-Greenfield <ajamesgreenfield@gmail.com>
2016-01-20 15:43:33 +0000

    programme works, but not for six trillion

--- third/app.js
+++ third/app.js
@@ -1,5 +1,19 @@
 var finalNum = 600851475143
-console.log(_.range(10))
-for(n = 2; n <= finalNum; n++ ){
-  // for([2..n])
+range = []
+
+
+for(n = 1; n < finalNum/2; n += 2 ){
+  var isNotPrime = false
+  if(finalNum % n == 0){
+    for(x = 3; x < n/2; x += 2){
+      if(n % x == 0){
+        isNotPrime = true
+      }
+    }
+    if(isNotPrime == false){
+      range.push(n) 
+    }
+  }
 }
+console.log(range)
+console.log(range[range.length - 1])
