3ec6364c3fe7ed929c439cb5e123d6c7a2bb5882
Adam-Greenfield <ajamesgreenfield@gmail.com>
2016-01-20 16:12:32 +0000

    found solution after using 71 as a starting prime factor

--- third/app.js
+++ third/app.js
@@ -2,18 +2,23 @@ var finalNum = 600851475143
 range = []
 
 
-for(n = 1; n < finalNum/2; n += 2 ){
-  var isNotPrime = false
-  if(finalNum % n == 0){
-    for(x = 3; x < n/2; x += 2){
-      if(n % x == 0){
-        isNotPrime = true
+// if(finalNum % 3 == 0 ){
+  var newNum = finalNum / ( 71 )
+  for(n = 1; n < newNum /2; n += 2 ){
+    var isNotPrime = false
+    if(newNum % n == 0){
+      for(x = 3; x < n/2; x += 2){
+        if(n % x == 0){
+          isNotPrime = true
+        }
+      }
+      if(isNotPrime == false){
+        range.push(n)
+        console.log(n)
       }
     }
-    if(isNotPrime == false){
-      range.push(n) 
-    }
   }
-}
-console.log(range)
-console.log(range[range.length - 1])
+  console.log(range)
+  console.log(range[range.length - 1])
+// }
+// else console.log("no dice")
