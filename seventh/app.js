1b13ca7ab8a0c8d5e971ca3737a20db8524d638e
Adam-Greenfield <ajamesgreenfield@gmail.com>
2016-01-21 10:25:30 +0000

    started over

--- seventh/app.js
+++ seventh/app.js
@@ -3,9 +3,11 @@ var check = Array.apply(null, {length: N}).map(Number.call, Number);
 var primeArray = [];
 
 check.forEach(function(c){
+  primeArray.push(check[0])
   if(c % 2 == 0){
     check.splice((check.indexOf(c)), 1)
   }
-})
-check.splice(0, 1);
-console.log(check);
+});
+
+console.log("check is " + check);
+console.log("prime array is " + primeArray);
