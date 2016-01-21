c1d31898980c9795a2b71e4d0f21f37fccfa658b
Adam-Greenfield <ajamesgreenfield@gmail.com>
2016-01-21 10:43:43 +0000

    skipping 7 for the moment

--- seventh/app.js
+++ seventh/app.js
@@ -1,10 +1,11 @@
 var N = 100; 
 var check = Array.apply(null, {length: N}).map(Number.call, Number);
+check.splice(0,2)
 var primeArray = [];
 
 check.forEach(function(c){
   primeArray.push(check[0])
-  if(c % 2 == 0){
+  if(c % (check[0]) == 0){
     check.splice((check.indexOf(c)), 1)
   }
 });
